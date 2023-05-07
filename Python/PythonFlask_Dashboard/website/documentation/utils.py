a56720fbb6c76f322062ee8b0ed6b71c61be9e86
Julian Stiebler <68881884+JulianStiebler@users.noreply.github.com>
2023-05-07 04:16:23 +0200

    Update utils.py

--- Python/PythonFlask_Dashboard/website/documentation/utils.py
+++ Python/PythonFlask_Dashboard/website/documentation/utils.py
@@ -13,13 +13,13 @@ def upload_file():
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            return redirect(url_for('documentation.documentation_home'))
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
-            return redirect(request.url)
+            return redirect(url_for('documentation.documentation_home'))
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
